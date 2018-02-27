704b8a90dcce42948d5ff7f5a08f2ff8ec14c4a2
yypbd_home <yypbd.dev@gmail.com>
2018-02-28 00:32:40 +0900

    Add initial project

---
+++ src/Form.VuejsLauncherMain.pas
@@ -0,0 +1,35 @@
+﻿unit Form.VuejsLauncherMain;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TFormVuejsLauncherMain = class(TForm)
+  private
+    { Private declarations }
+  protected
+    procedure DoCreate; override;
+  public
+    { Public declarations }
+  end;
+
+var
+  FormVuejsLauncherMain: TFormVuejsLauncherMain;
+
+implementation
+
+{$R *.dfm}
+
+{ TFormVuejsLauncherMain }
+
+procedure TFormVuejsLauncherMain.DoCreate;
+begin
+  inherited;
+
+  Caption := Application.Title;
+end;
+
+end.
---
+++ src/VuejsLauncher.dpr
@@ -0,0 +1,16 @@
+﻿program VuejsLauncher;
+
+uses
+  EMemLeaks,
+  Vcl.Forms,
+  Form.VuejsLauncherMain in 'Form.VuejsLauncherMain.pas' {FormVuejsLauncherMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.Title := 'Vue.js Launcher';
+  Application.CreateForm(TFormVuejsLauncherMain, FormVuejsLauncherMain);
+  Application.Run;
+end.
