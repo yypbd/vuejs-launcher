cdf523f452879d1a69b4189e5cadee89e6bdbc9c
yypbd <yypbd@naver.com>
2018-02-28 14:49:41 +0900

    Arrange MainForm

--- src/Form.VuejsLauncherMain.pas
+++ src/Form.VuejsLauncherMain.pas
@@ -5,33 +5,54 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Buttons, Vcl.ComCtrls,
-  Vcl.ExtCtrls, System.UITypes, DataStore.Project, Vcl.Menus;
+  Vcl.ExtCtrls, System.UITypes, DataStore.Project, Vcl.Menus, System.Actions,
+  Vcl.ActnList, Vcl.ToolWin, PngImage, Vcl.ImgList;
 
 type
   TFormVuejsLauncherMain = class(TForm)
-    BitBtnConfig: TBitBtn;
-    BitBtn1: TBitBtn;
-    Panel1: TPanel;
-    ListViewProject: TListView;
-    Panel2: TPanel;
-    BitBtnProjectAdd: TBitBtn;
-    BitBtnProjectUpdate: TBitBtn;
-    BitBtnProjectDelete: TBitBtn;
     PopupMenuProject: TPopupMenu;
     MenuItemRevealinExplorer: TMenuItem;
     MenuItemOpeninCommandprompt: TMenuItem;
     MenuItemOpenNodejsCommandprompt: TMenuItem;
-    procedure BitBtnConfigClick(Sender: TObject);
-    procedure BitBtn1Click(Sender: TObject);
-    procedure BitBtnProjectAddClick(Sender: TObject);
-    procedure BitBtnProjectUpdateClick(Sender: TObject);
-    procedure BitBtnProjectDeleteClick(Sender: TObject);
+    MainMenu: TMainMenu;
+    MenuItemFile: TMenuItem;
+    MenuItemFileExit: TMenuItem;
+    MenuItemProject: TMenuItem;
+    MenuItemProjectAdd: TMenuItem;
+    MenuItemProjectUpdate: TMenuItem;
+    MenuItemProjectDelete: TMenuItem;
+    MenuItemTools: TMenuItem;
+    MenuItemToolsOptions: TMenuItem;
+    ActionList: TActionList;
+    ActionFileExit: TAction;
+    ActionProjectAdd: TAction;
+    ActionProjectUpdate: TAction;
+    ActionProjectDelete: TAction;
+    ActionToolsSettings: TAction;
+    ToolBar: TToolBar;
+    ToolButtonProjectAdd: TToolButton;
+    ToolButtonProjectUpdate: TToolButton;
+    ToolButtonProjectDelete: TToolButton;
+    ToolButtonSep02: TToolButton;
+    ToolButtonToolsSettings: TToolButton;
+    ImageListAction: TImageList;
+    ListViewProject: TListView;
+    ActionProjectRun: TAction;
+    ToolButton1: TToolButton;
+    ToolButtonSep01: TToolButton;
     procedure ListViewProjectDblClick(Sender: TObject);
     procedure ListViewProjectContextPopup(Sender: TObject; MousePos: TPoint;
       var Handled: Boolean);
     procedure MenuItemRevealinExplorerClick(Sender: TObject);
     procedure MenuItemOpeninCommandpromptClick(Sender: TObject);
     procedure MenuItemOpenNodejsCommandpromptClick(Sender: TObject);
+    procedure MenuItemOptionsClick(Sender: TObject);
+    procedure ActionFileExitExecute(Sender: TObject);
+    procedure ActionToolsSettingsExecute(Sender: TObject);
+    procedure ActionProjectAddExecute(Sender: TObject);
+    procedure ActionProjectUpdateExecute(Sender: TObject);
+    procedure ActionProjectDeleteExecute(Sender: TObject);
+    procedure ActionProjectRunExecute(Sender: TObject);
   private
     { Private declarations }
     FProjectList: TProjectList;
@@ -61,57 +82,12 @@ uses
 
 { TFormVuejsLauncherMain }
 
-procedure TFormVuejsLauncherMain.AddProjectItem(
-  const AProjectItem: TProjectItem);
-var
-  ListItem: TListItem;
+procedure TFormVuejsLauncherMain.ActionFileExitExecute(Sender: TObject);
 begin
-  ListItem := ListViewProject.Items.Add;
-  ListItem.Caption := AProjectItem.Name;
-  ListItem.SubItems.Add( AProjectItem.Path );
-  ListItem.SubItems.Add( TFrameworkTypeStr[AProjectItem.FrameworkType] );
+  Close;
 end;
 
-procedure TFormVuejsLauncherMain.BitBtn1Click(Sender: TObject);
-begin
-  // D:\devel\proto\www
-  // vuejs_exam01
-
-  {
-  if DirectoryExists('D:\devel\proto\www\vuejs_exam01') then
-  begin
-    TCmdExecutor.Run( True, ['cd "D:\devel\proto\www\vuejs_exam01"', 'D:',
-                             'npm run dev'
-                            ] );
-  end
-  else
-  begin
-    TCmdExecutor.Run( True, ['npm install --global vue-cli',
-                             'cd "D:\devel\proto\www"', 'D:',
-                             'vue init webpack vuejs_exam01',
-                             'cd vuejs_exam01',
-                             'npm install',
-                             'npm run dev'
-                            ] );
-  end;
-  }
-end;
-
-procedure TFormVuejsLauncherMain.BitBtnConfigClick(Sender: TObject);
-var
-  FormConfig: TFormConfig;
-begin
-  FormConfig := TFormConfig.Create( nil );
-  try
-    if FormConfig.ShowModal = mrOk then
-    begin
-    end;
-  finally
-    FormConfig.Free;
-  end;
-end;
-
-procedure TFormVuejsLauncherMain.BitBtnProjectAddClick(Sender: TObject);
+procedure TFormVuejsLauncherMain.ActionProjectAddExecute(Sender: TObject);
 var
   FormProject: TFormProject;
   ProjectItem: TProjectItem;
@@ -140,7 +116,7 @@ begin
   end;
 end;
 
-procedure TFormVuejsLauncherMain.BitBtnProjectDeleteClick(Sender: TObject);
+procedure TFormVuejsLauncherMain.ActionProjectDeleteExecute(Sender: TObject);
 begin
   if ListViewProject.Selected = nil then
     Exit;
@@ -152,7 +128,28 @@ begin
   end;
 end;
 
-procedure TFormVuejsLauncherMain.BitBtnProjectUpdateClick(Sender: TObject);
+procedure TFormVuejsLauncherMain.ActionProjectRunExecute(Sender: TObject);
+var
+  ProjectItem: TProjectItem;
+begin
+  if ListViewProject.Selected = nil then
+    Exit;
+
+  ProjectItem := FProjectList.Items[ListViewProject.Selected.Index];
+
+  case ProjectItem.FrameworkType of
+    ftVuejs:
+    begin
+      TCmdVuejs.Run( ProjectItem.Name, ProjectItem.Path );
+    end;
+    ftNuxtjs:
+    begin
+      TCmdNuxtjs.Run( ProjectItem.Name, ProjectItem.Path );
+    end;
+  end;
+end;
+
+procedure TFormVuejsLauncherMain.ActionProjectUpdateExecute(Sender: TObject);
 var
   FormProject: TFormProject;
   ProjectItem: TProjectItem;
@@ -182,6 +179,31 @@ begin
   finally
     FormProject.Free;
   end;
+end;
+
+procedure TFormVuejsLauncherMain.ActionToolsSettingsExecute(Sender: TObject);
+var
+  FormConfig: TFormConfig;
+begin
+  FormConfig := TFormConfig.Create( nil );
+  try
+    if FormConfig.ShowModal = mrOk then
+    begin
+    end;
+  finally
+    FormConfig.Free;
+  end;
+end;
+
+procedure TFormVuejsLauncherMain.AddProjectItem(
+  const AProjectItem: TProjectItem);
+var
+  ListItem: TListItem;
+begin
+  ListItem := ListViewProject.Items.Add;
+  ListItem.Caption := AProjectItem.Name;
+  ListItem.SubItems.Add( AProjectItem.Path );
+  ListItem.SubItems.Add( TFrameworkTypeStr[AProjectItem.FrameworkType] );
 end;
 
 procedure TFormVuejsLauncherMain.DoCreate;
@@ -231,23 +253,21 @@ begin
 end;
 
 procedure TFormVuejsLauncherMain.ListViewProjectDblClick(Sender: TObject);
-var
-  ProjectItem: TProjectItem;
 begin
-  if ListViewProject.Selected = nil then
-    Exit;
+  ActionProjectRun.Execute;
+end;
 
-  ProjectItem := FProjectList.Items[ListViewProject.Selected.Index];
-
-  case ProjectItem.FrameworkType of
-    ftVuejs:
+procedure TFormVuejsLauncherMain.MenuItemOptionsClick(Sender: TObject);
+var
+  FormConfig: TFormConfig;
+begin
+  FormConfig := TFormConfig.Create( nil );
+  try
+    if FormConfig.ShowModal = mrOk then
     begin
-      TCmdVuejs.Run( ProjectItem.Name, ProjectItem.Path );
     end;
-    ftNuxtjs:
-    begin
-      TCmdNuxtjs.Run( ProjectItem.Name, ProjectItem.Path );
-    end;
+  finally
+    FormConfig.Free;
   end;
 end;
 
